768b073932cc6a85d72e237d681c00827378df98
mrsCandyBar <candicekbar@gmail.com>
2017-07-06 22:04:05 +0200

    Refactor globalMenuItems and add notes for completion

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,25 @@
-var globalMenuItems = createRouteObj(['home','about','edit','beers']);
+var globalMenuItems = ['home', 'about', 'edit', 'todo', 'beers'];
+    globalMenuItems = createRouteObj(globalMenuItems);
+
 angular.module('myApp', ['ngRoute', 'tabsComponent'])
 
   .config(function($routeProvider) {
+
+    // add option to replace templateUrl value
     globalMenuItems.forEach((item) => {
       $routeProvider.when('/' + item.page, {
+        controller: item.page + 'Controls',
         templateUrl: 'template/' + item.page + '.html'
       })
     });
    
     $routeProvider
       .otherwise({
-        redirectTo:'/'
+        redirectTo:'/home'
       });
   })
 
+  // MENU
   .controller('MenuControls', function() {
     let menu = this;
     menu.items = globalMenuItems;
@@ -24,8 +30,23 @@ angular.module('myApp', ['ngRoute', 'tabsComponent'])
     });
   })
 
-  .controller('TodoControls', function() {
-    var todoList = this;
+  // PAGES  
+  // Make this a dashboard
+  .controller('homeControls', function($scope) {
+    $scope.heading = 'Welcome';
+  })
+  .controller('aboutControls', function($scope) {
+    $scope.heading = 'About';
+  })
+
+  // Remove this and place as a option to edit data on dashboard
+  .controller('editControls', function($scope) {
+    $scope.heading = 'Edit ';
+  })
+
+  // REDO, make this fit into firebase
+  .controller('todoControls', function() {
+    let todoList = this;
     todoList.todos = [
       {text:'learn AngularJS test', done:true},
       {text:'build an AngularJS app', done:false}];
@@ -50,12 +71,12 @@ angular.module('myApp', ['ngRoute', 'tabsComponent'])
         if (!todo.done) todoList.todos.push(todo);
       });
     };
-
   })
 
-  .controller('BeersControls', function($scope, $locale) {
+  // replace this with some fun stats based on user data returned
+  .controller('beersControls', function($scope, $locale) {
     $scope.beers = [0, 1, 2, 3, 4, 5, 6];
-    
+
     if ($locale.id == 'en-us') {
       $scope.beerForms = {
         0: 'no beers',
